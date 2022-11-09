6505cb964c49f6c891907dc3cfb46551b74c0f4a
Zenon Nowak <zewak@interia.pl>
2022-11-09 16:14:46 +0100

    Treść  Zadania 14 - testowo

---
+++ src/main/java/ZadaniaDodatkowe/Zadanie14/Zadanie14Main.java
@@ -0,0 +1,22 @@
+package ZadaniaDodatkowe.Zadanie14;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Na podstawie 100000 elementowej tablicy z losowo wybranymi wartościami zaimplementuj następujące funkcjonalności:
+ * zwróć listę unikalnych elementów,
+ * zwróć listę elementów, które co najmniej raz powtórzyły się w wygenerowanej tablicy,
+ * zwróć listę 25 najczęściej powtarzających się elementów.
+ * Zaimplementuj metodę, która deduplikuje elementy w liście. W przypadku znalezienia duplikatu, zastępuje go nową losową wcześniej nie występującą wartością. Sprawdź czy metoda zadziałała poprawnie wywołując metodę numer 2.
+ */
+
+
+public class Zadanie14Main {
+    public static void main(String[] args) {
+        System.out.println("Zadanie14");
+        List<Integer> integerList = new ArrayList<>();
+
+
+    }
+}
